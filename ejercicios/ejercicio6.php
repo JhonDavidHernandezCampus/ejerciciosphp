7742144a62e1c26f20bb948b3959751ad691e649
JhonDavidHernandezCampus <yonif975@gmail.com>
2023-05-30 09:31:57 -0500

    feat: :sparkles: ajercicio 6 casi terminado
    Se termina la logica del ejercicio numero 6 solo falta  maquetar la tabla

--- ejercicios/ejercicio6.php
+++ ejercicios/ejercicio6.php
@@ -5,13 +5,13 @@
         public $W = [];
         public $D = [];
         public $L = [];
-        public function __construct($scores)
-        {
+        public $P = [];
+        public function __construct($scores){
             $this->equipos = explode(";", $scores);
 
         }
         public function asignacionPuntos(){
-            var_dump($this->equipos);
+            //var_dump($this->equipos);
             foreach ($this->equipos as $key => $value) {
                 if($key%3 == 2){
                     switch ($this->equipos[$key]) {
@@ -20,18 +20,25 @@
                             $nombreEquipo2 = $this->equipos[$key-1];
                             ($this->W[$nombreEquipo] ?? null) ? $this->W[$nombreEquipo] += 1 : $this->W[$nombreEquipo] = 1;
                             ($this->L[$nombreEquipo2] ?? null) ? $this->L[$nombreEquipo2] += 1 : $this->L[$nombreEquipo2] = 1;
+                            ($this->P[$nombreEquipo2] ?? null) ? $this->P[$nombreEquipo2] += 1 : $this->P[$nombreEquipo2] = 3;
                             break;
                         case 'draw':
                             $nombreEquipo = $this->equipos[$key-2];
                             $nombreEquipo2 = $this->equipos[$key-1];
                             ($this->D[$nombreEquipo] ?? null) ? $this->D[$nombreEquipo] += 1 : $this->D[$nombreEquipo] = 1;
                             ($this->D[$nombreEquipo2] ?? null) ? $this->D[$nombreEquipo2] += 1 : $this->D[$nombreEquipo2] = 1;
+
+                            ($this->P[$nombreEquipo] ?? null) ? $this->P[$nombreEquipo] += 1 : $this->P[$nombreEquipo] = 1;
+                            ($this->P[$nombreEquipo2] ?? null) ? $this->P[$nombreEquipo2] += 1 : $this->P[$nombreEquipo2] = 1;
+
                             break;
                         case 'loss':
                             $nombreEquipo = $this->equipos[$key-1];
                             $nombreEquipo2 = $this->equipos[$key-2];
                             ($this->W[$nombreEquipo] ?? null) ? $this->W[$nombreEquipo] += 1 : $this->W[$nombreEquipo] = 1;
                             ($this->L[$nombreEquipo2] ?? null) ? $this->L[$nombreEquipo2] += 1 : $this->L[$nombreEquipo2] = 1;
+                            ($this->P[$nombreEquipo] ?? null) ? $this->P[$nombreEquipo] += 1 : $this->P[$nombreEquipo] = 3;
+
                             break;
                     }
                 }else{
@@ -39,13 +46,39 @@
                 }
             }
         }
+        public function validarEquipos(){
+            $equiposFaltantesW = array_diff_key($this->MP,$this->W);
+            foreach($equiposFaltantesW as $key => $value){
+                $this->W[$key];
+            }
+            $equiposFaltantesD = array_diff_key($this->MP,$this->D);
+            foreach($equiposFaltantesD as $key => $value){
+                $this->D[$key];
+            }
+            $equipósFaltantesL = array_diff_key($this->MP,$this->L);
+            foreach($equiposFaltantesL as $key => $value){
+                $this->L[$key];
+            }
+            $equipósFaltantesP = array_diff_key($this->MP,$this->P);
+            foreach($equiposFaltantesW as $key => $value){
+                $this->P[$key];
+            }
+        }
+        public function pintarTabla(){
+            $cant = count($this->P);
+            echo "Team \t\t\t | MP | W | D | L | P | ";
+            foreach ($this->D as $key => $value) {
+                echo "\n"   .$value."\n";
+            }
+
+
+
+        }
     }
     $obj = new Tournament("Allegoric Alaskans;Blithering Badgers;win;Devastating Donkeys;Courageous Californians;draw;Devastating Donkeys;Allegoric Alaskans;win;Courageous Californians;Blithering Badgers;loss;Blithering Badgers;Devastating Donkeys;loss;Allegoric Alaskans;Courageous Californians;win");
     $obj->asignacionPuntos();
-    var_dump($obj->MP);
-    var_dump($obj->W);
-    var_dump($obj->D);
-    var_dump($obj->L);
+    $obj->validarEquipos();
+    $obj->pintarTabla();
 ?>
 
 
